830ca5ab5b34b02c35bd3ad2650d5d567e875876
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-07-18 20:24:52 -0400

    github professional account is different icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer color="black" class="pa-3">
     <a href="https://github.com/drwhirstyburgers" target="_blank"><v-icon color="white">fab fa-github</v-icon></a>
-    <a href="https://github.com/drwhirst" target="_blank"><v-icon color="white" class="icons">fab fa-github</v-icon></a>
+    <a href="https://github.com/drwhirst" target="_blank"><v-icon color="white" class="icons">fab fa-github-square</v-icon></a>
     <a href="https://twitter.com/Thursty20" target="_blank"><v-icon color="white" class="icons">fab fa-twitter</v-icon></a>
     <a href="https://www.linkedin.com/in/drwhirst/" target="_blank"><v-icon color="white" class="icons">fab fa-linkedin</v-icon></a>
     <v-spacer></v-spacer>
