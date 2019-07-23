678b67b55c7a6c43f0dde88bb58a9ef39f895b36
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-07-23 18:45:50 -0400

    format changes

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,61 +3,45 @@
     <div class="full-screen">
         <transition name="slide-fade">
         <v-layout align-center justify-center row fill-height fluid v-if="projectOne">
-            <v-layout id="l1">
-                <v-flex id="b1" xs2>
+            <v-layout class="l1">
+                <v-flex class="b1" xs2>
                     <v-flex tag="h2" id="title">{{project.title}}</v-flex>
                     <v-icon  v-for="icon in project.languages" v-bind:icon="icon" v-bind:key="icon.id" color="white" id="icon">{{ icon }}</v-icon>
                     <v-flex tag="h3">{{project.role}}</v-flex>
                 </v-flex>
-                <v-flex id="b2" xs8>
+                <v-flex class="b2" xs8>
                     <v-flex tag="p"><strong><u>Description:</u></strong>  {{project.description}} </br> <strong><u>Challenge:</u></strong>  {{project.challenges}} </br> <strong><u>Solutions:</u></strong>  {{project.solutions}} </br> <strong><u>Future Considerations:</u></strong>  {{project.futureConsiderations}} </v-flex>
                 </v-flex>
             </v-layout>
-            <v-layout row>
-                <v-flex xs6 id="c2">
-                    <v-spacer></v-spacer><v-icon v-ripple v-if="!projectThree" @click="nextProject()" color="white" size="50px">chevron_right</v-icon>
-                </v-flex>
-            </v-layout>
+            <v-icon v-ripple v-if="!projectThree" @click="nextProject()" color="white" size="50px">chevron_right</v-icon>
         </v-layout>
         </transition>
         <transition name="slide-fade">
         <v-layout align-center justify-center row fill-height fluid v-if="projectTwo" transition="expand">
-            <v-layout row>
-                <v-flex xs6>
-                    <v-icon v-ripple color="white" size="50px" id="c1" v-if="!projectOne" @click="previousProject()">chevron_left</v-icon>
-                </v-flex>
-            </v-layout>
-            <v-layout id="l1">
-                <v-flex id="b1" xs2>
+            <v-icon v-ripple color="white" size="50px" id="c1" @click="previousProject()">chevron_left</v-icon>
+            <v-layout class="l1">
+                <v-flex class="b1" xs2>
                     <v-flex tag="h2" id="title">{{project2.title}}</v-flex>
                     <v-icon  v-for="icon in project2.languages" v-bind:icon="icon" v-bind:key="icon.id" color="white" id="icon">{{ icon }}</v-icon>
                     <v-flex tag="h3">{{project2.role}}</v-flex>
                 </v-flex>
-                <v-flex id="b2" xs8>
+                <v-flex class="b2" id="mid" xs8>
                     <v-flex tag="p"><strong><u>Description:</u></strong>  {{project2.description}} </br> <strong><u>Challenge:</u></strong>  {{project2.challenges}} </br> <strong><u>Solutions:</u></strong>  {{project2.solutions}} </br> <strong><u>Future Considerations:</u></strong>  {{project2.futureConsiderations}} </v-flex>
                 </v-flex>
             </v-layout>
-            <v-layout row>
-                <v-flex xs6 id="c2">
-                    <v-spacer></v-spacer><v-icon v-ripple v-if="!projectThree" @click="nextProject()" color="white" size="50px">chevron_right</v-icon>
-                </v-flex>
-            </v-layout>
+            <v-icon v-ripple @click="nextProject()" color="white" size="50px">chevron_right</v-icon>
         </v-layout>
         </transition>
         <transition name="slide-fade">
         <v-layout align-center justify-center row fill-height fluid v-if="projectThree" transition="expand">
-                <v-layout row>
-                    <v-flex xs6>
-                        <v-icon v-ripple color="white" size="50px" id="c1" v-if="!projectOne" @click="previousProject()">chevron_left</v-icon>
-                    </v-flex>
-                </v-layout>
-            <v-layout id="l1">
-                <v-flex id="b1" xs2>
+            <v-icon v-ripple color="white" size="50px" id="c1" @click="previousProject()">chevron_left</v-icon>
+            <v-layout class="l1">
+                <v-flex class="b1" xs2>
                     <v-flex tag="h2" id="title">{{project3.title}}</v-flex>
                     <v-icon  v-for="icon in project3.languages" v-bind:icon="icon" v-bind:key="icon.id" color="white" id="icon">{{ icon }}</v-icon>
                     <v-flex tag="h3">{{project3.role}}</v-flex>
                 </v-flex>
-                <v-flex id="b2" xs8>
+                <v-flex class="b2" xs8>
                     <v-flex tag="p"><strong><u>Description:</u></strong>  {{project3.description}} </br> <strong><u>Challenge:</u></strong>  {{project3.challenges}} </br> <strong><u>Solutions:</u></strong>  {{project3.solutions}} </br> <strong><u>Future Considerations:</u></strong>  {{project3.futureConsiderations}} </v-flex>
                 </v-flex>
             </v-layout>
@@ -126,6 +110,13 @@ export default {
                 this.projectTwo = true
                 this.projectCount = 1
             }
+        },
+        isMobile() {
+            if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                return true
+            } else {
+                return false
+            }
         }
     }
 }
@@ -146,14 +137,15 @@ export default {
 h3 {
     color: grey;
 }
-#b1 {
+.b1 {
+    display: block;
     text-align: right;
     margin-right: 5px;
 }
-#b2 {
-    margin-left: 10px;
+.b2 {
+    margin-left: 17px;
 }
-#l1 {
+.l1 {
     min-width: 270px;
 }
 #title{
